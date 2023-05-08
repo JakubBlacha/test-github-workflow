89b746e75a28fbc43ddd9cb03faa74f94a22626e
JakubBlacha <114864986+JakubBlacha@users.noreply.github.com>
2023-05-08 15:04:56 +0200

    test

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+"""imports"""
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """function"""
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
 
 if __name__ == "__main__":
